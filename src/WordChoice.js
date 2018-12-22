b002a3bb058732dc8e03532b9909d75e7f856389
Jessica Bunyan <tristan@bunyan.com>
2018-12-22 06:43:06 +0000

    more polish on styling, more data

--- src/WordChoice.js
+++ src/WordChoice.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import * as d3 from "d3";
 import * as _ from "underscore";
 
-
-
-class WordChoice extends Component{
-
-    
-
-      render(){
-        return (
-        <div className="word-choice" onClick={() => this.props.onClick(this.props.word)}>
-            {this.props.word}
-        </div>
-        )  
-    }
-
-    }
-
+class WordChoice extends Component {
+  render() {
+    return (
+      <div
+        className="word-choice"
+        onClick={() => this.props.onClick(this.props.word)}
+      >
+        {this.capitalise(this.props.word)}
+      </div>
+    );
+  }
+  capitalise(word) {
+    return word.substring(0, 1).toUpperCase() + word.substring(1);
+  }
+}
 
 export default WordChoice;
--- src/WordPanel.js
+++ src/WordPanel.js
@@ -7,7 +7,7 @@ class WordPanel extends Component {
   render() {
     return (
       <div className="word-panel clearfix">
-        <h4>Choose a word or phrase</h4>
+        <h2>Choose a word or phrase</h2>
         {this.props.words.map(e => {
           return <WordChoice word={e} onClick={w => this.props.onClick(w)} />;
         })}

5c8e31e7ca01be01f8599febb32a7461f192425b
Jessica Bunyan <tristan@bunyan.com>
2019-01-16 21:40:28 +0000

    working prototype for Ice and the wall

--- src/WordChoice.js
+++ src/WordChoice.js
@@ -6,13 +6,18 @@ import { capitalise } from "./utils";
 class WordChoice extends Component {
     render() {
         return (
-            <div
-                className="word-choice"
-                onClick={() => this.props.onClick(this.props.word)}
-            >
+            <div className={this.getClass()} onClick={() => this.props.onClick(this.props.word)}>
                 {capitalise(this.props.word)}
             </div>
         );
+    }
+
+    getClass() {
+        var word = this.props.word.toLowerCase();
+        if (word === "ice" || word === "the wall") {
+            return "word-choice blue";
+        }
+        return "word-choice ";
     }
 }
 

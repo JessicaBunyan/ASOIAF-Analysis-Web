2f51d55c66f4d6c8215a63375505f6513c4b7ada
Jessica Bunyan <tristan@bunyan.com>
2019-01-27 01:13:46 +0000

    wip

--- src/graph.js
+++ src/graph.js
@@ -2,39 +2,26 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 import * as _ from "underscore";
 import { capitalise } from "./utils";
+import Canvas from "./Canvas";
 
-const leftMargin = 80;
-const topMargin = 10;
-const MaxBarWidth = 80;
-const canvasWidth = 1200;
-const axisLabelFontSize = 20;
 const singleBookOffset = 20;
 const barPadding = 0.05;
-const bookColours = [
-    "",
-    "rgba(0,0,255,0.4)",
-    "rgba(255,255,0,0.4)",
-    "rgba(0,255,0,0.4)",
-    "rgba(255,0,0,0.4)",
-    "rgba(255,255,255,0.4)"
-];
-const bookImgIds = ["", "agot-cover", "acok-cover", "asos-cover", "affc-cover", "adwd-cover"];
-const bookImgLeftIds = ["", "agot-left", "acok-left", "asos-left", "affc-left", "adwd-left"];
-const bookImgRightIds = ["", "agot-right", "acok-right", "asos-right", "affc-right", "adwd-right"];
-
-const baseBookImgWidth = 630;
-const baseBookImgHeight = 961;
-
-const bookImgOffsets = [0, 100, 200, 200, 200, 100];
+const MaxBarWidth = 80;
+const leftMargin = 80;
+const topMargin = 10;
 
 var elements = [];
 
+const CANVAS_WIDTH = 1200;
+const CANVAS_HEIGHT = 800;
+
 class Graph extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
     componentDidMount() {
         this.createBarChart();
-        var canvas = document.querySelector("canvas");
-        canvas.addEventListener("click", e => this.onChartClick(e));
-        canvas.addEventListener("mousemove", e => this.onChartHover(e));
     }
     componentDidUpdate() {
         this.clearCanvas();
@@ -42,9 +29,18 @@ class Graph extends Component {
     }
 
     render() {
+        if (!this.state.readyToDraw) {
+            return null;
+        }
         return (
             <div className="canvasWrapper">
-                <canvas width={canvasWidth} height="800" />
+                <Canvas
+                    width={CANVAS_WIDTH + "px"}
+                    height={CANVAS_HEIGHT + "px"}
+                    y={this.state.y}
+                    x={this.state.x}
+                    yTicks={this.state.yTicks}
+                />
             </div>
         );
     }
@@ -96,19 +92,12 @@ class Graph extends Component {
         }
     }
 
-    clearCanvas() {
-        var canvas = document.querySelector("canvas");
-        var context = canvas.getContext("2d");
-        context.clearRect(0, 0, 10000, 10000);
-    }
     createBarChart() {
         var data = this.props.data;
-        var canvas = document.querySelector("canvas"),
-            context = canvas.getContext("2d");
 
         var margin = { top: 20, right: 20, bottom: 300, left: 60 },
-            width = canvas.width - margin.left - margin.right,
-            height = canvas.height - margin.top - margin.bottom;
+            width = CANVAS_WIDTH - margin.left - margin.right,
+            height = CANVAS_HEIGHT - margin.top - margin.bottom;
 
         // if we're breaking down by chapter allow room for gaps between books
         width = this.props.breakdown ? width - 4 * singleBookOffset : width;
@@ -132,159 +121,50 @@ class Graph extends Component {
         console.log("Y TICK COUNT: " + yTickCount);
         var yTicks = y.ticks(yTickCount);
 
-        context.fillStyle = "black";
-        this.drawXAxisTicks(context, x, height);
-        this.drawXAxisLine(context, height);
-        this.drawXAxisLabels(context, x, height);
+        // this.drawXAxisTicks(context, x, height);
+        // this.drawXAxisLine(context, height);
+        // this.drawXAxisLabels(context, x, height);
 
-        this.drawYAxisTicks(context, yTicks, y);
-        this.drawYAxisLine(context, height);
-        this.drawYAxisLabels(context, yTicks, y);
+        // this.drawYAxisTicks(context, yTicks, y);
+        // this.drawYAxisLine(context, height);
+        // this.drawYAxisLabels(context, yTicks, y);
 
-        this.drawBookBoundaries(context, x, width, height);
-        this.drawBars(context, data, x, y, height);
+        // this.drawBookBoundaries(context, x, width, height);
+        var bars = this.calculateBars(x, y, data, height);
+
+        this.setState({
+            bars: bars,
+            x: x,
+            y: y,
+            yTicks: yTicks,
+            readyToDraw: true
+        });
     }
 
-    drawBookBoundaries(context, x, canvasWidth, height) {
-        if (!this.props.breakdown) {
-            return;
-        }
+    calculateBars(x, y, data, chartHeight) {
+        elements = [];
+        // Bars
 
-        var barWidth = x.bandwidth();
-
-        if (barWidth > MaxBarWidth) {
+        var width = x.bandwidth();
+        var diff = 0;
+        if (width > MaxBarWidth) {
             // if we're limiting the bar width we need to adjust the left position to account for the difference
-            barWidth = MaxBarWidth;
+            diff = width - MaxBarWidth;
+            width = MaxBarWidth;
         }
 
-        var bookInfo = this.getChaptersPerBook();
+        Object.keys(data).forEach(d => {
+            var el = {
+                char: d,
+                left: this.getBarLeft(x, d, width),
+                top: y(data[d]) + topMargin,
+                width: width,
+                height: chartHeight - y(data[d])
+            };
+            elements.push(el);
+        });
 
-        var boundaryChapters = [];
-
-        var first = -1,
-            last = -1;
-        for (var k = 1; k < 6; k++) {
-            console.log(bookInfo[k]);
-            if (bookInfo[k].length) {
-                // if it has chapters
-                first = bookInfo[k][0];
-                last = bookInfo[k][bookInfo[k].length - 1];
-                boundaryChapters.push(first);
-                boundaryChapters.push(last);
-            } else {
-                boundaryChapters.push(last); // if its at the start, we will put -1s in,
-                boundaryChapters.push(last); // otherwise we will repeat previous ones
-            }
-        }
-
-        var barCenters = boundaryChapters.map(c => (c == -1 ? -1 : this.getBarLeft(x, c) + barWidth / 2));
-        var midPoints = [0];
-
-        console.log("boundary chapters");
-        console.log(boundaryChapters);
-        console.log(barCenters);
-
-        for (var i = 1; i < barCenters.length - 1; i += 2) {
-            var endBar = barCenters[i];
-            var startBar = barCenters[i + 1];
-
-            console.log("bars: " + endBar + ", " + startBar);
-            console.log("midpoints so far: ");
-            console.log(midPoints);
-
-            var midPoint = startBar - leftMargin + (endBar - startBar) / 2;
-
-            if (endBar == startBar) {
-                midPoint = -2; // -2 aka missing chapter but not at start
-            }
-            if (barCenters[i] == -1 && barCenters[i - 1] == -1) {
-                midPoint = -1; // -1 aka missing chapter at start
-            }
-
-            console.log("new midpoint");
-            console.log(midPoint);
-            midPoints.push(midPoint);
-        }
-
-        midPoints.push(canvasWidth + 4 * singleBookOffset);
-
-        console.log("boundary chapters");
-        console.log(boundaryChapters);
-        console.log(barCenters);
-        console.log("midpoints");
-        console.log(midPoints);
-
-        // first deal with all the -1s - simply add gaps counting up from 0
-        for (var i = 1; i < midPoints.length - 1; i++) {
-            if (midPoints[i] == -1) {
-                midPoints[i] = midPoints[i - 1] + singleBookOffset;
-            }
-        }
-
-        //deal with all the -2s
-        for (var i = midPoints.length - 1; i >= 0; i--) {
-            if (midPoints[i] == -2) {
-                midPoints[i] = midPoints[i + 1] - singleBookOffset;
-                if (midPoints[midPoints.length - 1] - midPoints[i] >= 41) {
-                    // hacky way of checking if there's books with chapters after this or not
-                    midPoints[i] += singleBookOffset / 2;
-                    midPoints[i + 1] += singleBookOffset / 2;
-                }
-            }
-        }
-        console.log(midPoints);
-
-        for (var book = 1; book < 6; book++) {
-            this.drawBook(context, midPoints, book, height + topMargin + 180);
-        }
-
-        var img = document.getElementById("agot-cover");
-        context.fillStyle = "black";
-
-        context.globalAlpha = 1;
-    }
-
-    drawBook(context, midPoints, bookNum, height) {
-        var left = midPoints[bookNum - 1];
-        var width = midPoints[bookNum] - left;
-        left = left + leftMargin;
-
-        context.fillStyle = bookColours[bookNum];
-        var img = document.getElementById(bookImgIds[bookNum]);
-        context.globalAlpha = 0.2;
-
-        if (width <= singleBookOffset + 2) {
-            // If there are no chapters we just draw a coloured line/strip
-            context.fillRect(left, 0, width, height);
-        } else {
-            if (width < 550) {
-                // if its not too wide just draw the image as is
-                context.fillStyle = "black";
-                context.drawImage(img, 0, 0, baseBookImgWidth, baseBookImgHeight, left, 0, width, height);
-            } else {
-                // if its too wise we draw the image in the middle and "fill in" the sides
-                var fillAreaWidth = (width - 550) / 2;
-                var leftImg = document.getElementById(bookImgLeftIds[bookNum]);
-                var rightImg = document.getElementById(bookImgRightIds[bookNum]);
-
-                this.fillInBookSide(context, leftImg, left, fillAreaWidth, height);
-                this.fillInBookSide(context, rightImg, left + 550 + fillAreaWidth, fillAreaWidth, height);
-
-                context.drawImage(img, 0, 0, baseBookImgWidth, baseBookImgHeight, left + fillAreaWidth, 0, 550, height);
-            }
-        }
-        context.fillStyle = "black";
-    }
-
-    fillInBookSide(context, img, left, fillWidth, height) {
-        for (var i = 0; i < fillWidth; i += 10) {
-            var tileWidth = 10;
-            if (i > fillWidth - 10) {
-                // if we have less than 10 pixels to the end shorten the tile width so we don't overflow
-                tileWidth = fillWidth - i;
-            }
-            context.drawImage(img, 0, 0, 10, 961, left + i, 0, tileWidth, height);
-        }
+        return elements;
     }
 
     getChaptersPerBook() {
@@ -344,123 +224,6 @@ class Graph extends Component {
         );
 
         return yTickCount;
-    }
-
-    drawXAxisTicks(context, x, height) {
-        // X-axis "ticks"
-        context.lineWidth = 1;
-        context.beginPath();
-        x.domain().forEach(d => {
-            var bookOffset = this.getBookOffset(d);
-            var xPos = leftMargin + bookOffset + x(d) + x.bandwidth() / 2;
-            context.moveTo(xPos, height + topMargin);
-            context.lineTo(xPos, height + 6 + topMargin);
-        });
-        context.strokeStyle = "black";
-        context.stroke();
-    }
-    drawXAxisLine(context, height) {
-        context.lineWidth = 4;
-        context.beginPath();
-        context.moveTo(leftMargin, height + topMargin + 2);
-        context.lineTo(canvasWidth, height + topMargin + 2);
-        context.strokeStyle = "black";
-        context.stroke();
-    }
-    drawXAxisLabels(context, x, height) {
-        // X-axis labels (character/chapter names)
-        context.textAlign = "left";
-        context.textBaseline = "top";
-        context.font = axisLabelFontSize + "px Arial";
-
-        console.log("drawing x axis label");
-        x.domain().forEach(d => {
-            context.translate(0, 0); // make sure we're back here before rotating
-            context.save();
-
-            context.rotate(Math.PI / 2); // turn the paper anticlockwise
-            context.fillText(
-                this.props.lookupXAxisLabel(d),
-                height + topMargin + 8, // our y value for the location we want is now "x" here (will match up when we rotate paper backwards)
-                -(leftMargin + x(d) + this.getBookOffset(d) + axisLabelFontSize / 2 + x.bandwidth() / 2) // the negative of our x value is now our "y"
-            );
-            context.restore();
-        });
-    }
-
-    drawYAxisTicks(context, yTicks, y) {
-        // y-axis "ticks" delineating axis
-        context.lineWidth = 1;
-        context.beginPath();
-        console.log("y ticks");
-        console.log(yTicks);
-        yTicks.forEach(function(d) {
-            context.moveTo(leftMargin, y(d) + 0.5 + topMargin);
-            context.lineTo(leftMargin - 5, y(d) + 0.5 + topMargin);
-        });
-        context.strokeStyle = "black";
-        context.stroke();
-    }
-    drawYAxisLine(context, height) {
-        // Vertical Axis (line)
-        context.beginPath();
-        context.lineWidth = 4;
-        context.moveTo(leftMargin, topMargin);
-        context.lineTo(leftMargin, height + topMargin + 4); // 4 for linewidth
-        context.strokeStyle = "black";
-        context.stroke();
-    }
-    drawYAxisLabels(context, yTicks, y) {
-        // y-axis scale labels
-        context.textAlign = "right";
-        context.textBaseline = "middle";
-        context.font = axisLabelFontSize + "px Arial";
-        yTicks.forEach(function(d) {
-            context.fillText(d, leftMargin - 10, y(d) + topMargin);
-        });
-
-        //// Occurences label
-
-        // context.rotate(Math.PI / 2);
-        // context.textAlign = "center";
-        // context.fillText("# Occurrences", topMargin + 200, -10);
-        // context.rotate(-Math.PI / 2);
-    }
-
-    drawBars(context, data, x, y, chartHeight) {
-        elements = [];
-        // Bars
-        context.fillStyle = "steelblue";
-
-        var width = x.bandwidth();
-        var diff = 0;
-        if (width > MaxBarWidth) {
-            // if we're limiting the bar width we need to adjust the left position to account for the difference
-            diff = width - MaxBarWidth;
-            width = MaxBarWidth;
-        }
-
-        Object.keys(data).forEach(d => {
-            var el = {
-                char: d,
-                left: this.getBarLeft(x, d, width),
-                top: y(data[d]) + topMargin,
-                width: width,
-                height: chartHeight - y(data[d])
-            };
-            elements.push(el);
-
-            this.drawBar(context, el, "black", "steelblue");
-        });
-
-        console.log(elements);
-    }
-
-    drawBar(context, el, colour, outline) {
-        context.fillStyle = colour;
-        context.fillRect(el.left, el.top, el.width, el.height);
-        context.fillStyle = outline;
-        context.fillRect(el.left + 1, el.top + 1, el.width - 2, el.height);
     }
 
     getBarLeft(x, d) {

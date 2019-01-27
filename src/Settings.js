189cc3bc94b4007a0563d185178d1372cfe2b8f7
Jessica Bunyan <tristan@bunyan.com>
2019-01-27 03:46:11 +0000

    settings fiel

---
+++ src/Settings.js
@@ -0,0 +1,15 @@
+const settings = {
+    leftMargin: 80,
+    topMargin: 10,
+    axisLabelFontSize: 20,
+    canvasWidth: 1200,
+    canvasHeight: 800,
+    singleBookOffset: 20,
+    maxBarWidth: 80,
+    barPadding: 0.05,
+
+    baseBookImgWidth: 630,
+    baseBookImgHeight: 961
+};
+
+export default settings;
